d0e2ca5dee89c616558373fca1bf7602dfebb4a7
Dhruv Suva <dhruvahir5959@gmail.com>
2025-04-13 16:27:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 const app = express();
 app.use(express.json());
+require("dotenv").config();
 
 // Import handlers dynamically
 const phone1Handler = require("./phone1Handler");
